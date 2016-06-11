08705954b05f2dd3234562545fa891a8d086ef95
Hiroaki Nakamura <hnakamur@gmail.com>
2016-06-11 10:26:40 +0900

    Remove serveHome

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,31 +9,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"text/template"
 
 	"github.com/hnakamur/ltsvlog"
 )
 
 var addr = flag.String("addr", ":8080", "http service address")
-var homeTemplate = template.Must(template.ParseFiles("home.html"))
-
-func serveHome(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
-		return
-	}
-	if r.Method != "GET" {
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	homeTemplate.Execute(w, r.Host)
-}
 
 func main() {
 	flag.Parse()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
 	http.HandleFunc("/work", serveWork)
 	http.HandleFunc("/ws", serveWs)
 	fmt.Printf("server start listening %s...\n", *addr)
